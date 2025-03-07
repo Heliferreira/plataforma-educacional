81e67696c08d483c3a11cf666c4e014a82e62a79
Heliferreira <heliveltonsouza@outlook.com>
2025-03-07 16:49:50 -0300

    corrigindo problemas de implantaçao

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.URL_DO_BANCO_DE_DADOS,
+
     ssl: {
         rejectUnauthorized: false, // Necessário para conexões seguras
     }
